77ae919fc66d38149426f54f6fdd10801dacce45
Umur Karagöz <lazysheepherd@gmail.com>
2017-09-09 02:18:01 +0300

    added asset publishing

--- src/MetamanServiceProvider.php
+++ src/MetamanServiceProvider.php
@@ -13,9 +13,17 @@ class MetamanServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->loadMigrationsFrom(__DIR__ . '/migrations');
+        
+        $this->publishes([__DIR__ . '/migrations/' => database_path('migrations')], 'migrations');
+        
+        $this->publishes([__DIR__ . '/Meta.php' => app_path('Models/Meta.php')], 'models');
+        
+        $this->publishes([__DIR__ . '/FeedsMeta.php' => app_path('Traits/FeedsMeta.php')], 'traits');
+        
+        $this->publishes([__DIR__ . '/config/' => config_path('')], 'config');
     }
-
+    
     /**
      * Register the application services.
      *
