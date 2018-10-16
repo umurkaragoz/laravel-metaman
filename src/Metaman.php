1d01c8044382d795b2a61f5218aee5ecf96842d6
Umur Karagoz <Lazysheepherd@gmail.com>
2018-10-16 12:27:19 +0300

    Added canonical tag support. Added secondard function signature for 'feed' method to accept ($name, $content) style input.

--- src/Metaman.php
+++ src/Metaman.php
@@ -7,56 +7,73 @@ use InvalidArgumentException;
 class Metaman
 {
     private static $metaData = [];
-    
+
     /**
-     * @param $input array|FeedsMeta
+     * Feed meta for the page.
+     *
+     * If the second parameter is supplied, parameters are name and content pair.
+     * Otherwise the first parameter is key value pair, either array or object implementing FeedsMeta.
+     *
+     * @param $nameOrData    string|array|FeedsMeta
+     * @param $content       string
      *
      * @return bool
      *
      * @throws InvalidArgumentException
      */
-    public static function feed($input)
+    public static function feed($nameOrData, $content = null)
     {
-        if (is_object($input)) {
-            $meta = $input->meta()->pluck('meta_content', 'meta_name')->toArray();
+        $meta = [];
+
+        if ($content) {
+            $meta[$nameOrData] = $content;
         } else {
-            $meta = $input;
+            if (is_object($nameOrData)) {
+                $meta = $nameOrData->meta()->pluck('meta_content', 'meta_name')->toArray();
+            } else {
+                $meta = $nameOrData;
+            }
         }
-        
+
         if (!is_array($meta)) {
             throw new InvalidArgumentException('Input must either be an array or a model using "FeedsMeta" trait.');
         }
-        
+
         self::$metaData = array_merge(self::$metaData, $meta);
     }
-    
+
     public static function render()
     {
         $meta = "\n";
         // extend project-wide defaults
         self::$metaData = array_merge(config('meta.defaults', []), self::$metaData);
-        
+
         foreach (self::$metaData as $name => $content) {
             $property = '';
             if (strpos($name, 'og:') === 0) {
                 $property = " property=\"$name\"";
             }
-            
+
             // process variables/inner links
             if (strpos($content, ':') === 0) {
                 $content = array_get(self::$metaData, substr($content, 1), false);
                 if (!$content) continue;
             }
-            
+
             // use 'title' tag instead if 'meta title'
             if ($name == 'title') {
                 $meta .= "    <title>$content</title>\n";
                 continue;
             }
-            
+            // use 'link' tag for canonical links
+            if ($name == 'canonical') {
+                $meta .= "    <link rel='canonical' href='$content'/>\n";
+                continue;
+            }
+
             $meta .= "    <meta$property name=\"$name\" content=\"$content\"/>\n";
         }
-        
+
         return $meta;
     }
 }
