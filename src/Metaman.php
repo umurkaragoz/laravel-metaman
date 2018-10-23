cc80074062abcfc6472fa6560ef0d00e9a8d2fb8
Umur Karagoz <Lazysheepherd@gmail.com>
2018-10-23 11:11:38 +0300

    Added SEO 'prev' and 'next' support.

--- src/Metaman.php
+++ src/Metaman.php
@@ -11,29 +11,44 @@ class Metaman
     /**
      * Feed meta for the page.
      *
-     * If the second parameter is supplied, parameters are name and content pair.
-     * Otherwise the first parameter is key value pair, either array or object implementing FeedsMeta.
+     * If the second parameter is supplied, parameters must be name and content respectively.
+     * If the second parameter is left null, the first parameter must be one of the below;
+     * - array to supply a list of key value pairs,
+     * - object implementing FeedsMeta trait, to feed modal meta,
+     * - paginated eloquent query result, for feeding 'next' and 'previous' page tags for SEO,
      *
-     * @param $nameOrData    string|array|FeedsMeta
-     * @param $content       string
+     * @param $nameOrOther    string|array|FeedsMeta|object
+     * @param $content        string
      *
      * @return bool
      *
      * @throws InvalidArgumentException
      */
-    public static function feed($nameOrData, $content = null)
+    public static function feed($nameOrOther, $content = null)
     {
         $meta = [];
 
         if ($content) {
-            $meta[$nameOrData] = $content;
+            $meta[$nameOrOther] = $content;
         } else {
-            if (is_object($nameOrData)) {
-                $meta = $nameOrData->meta()->pluck('meta_content', 'meta_name')->toArray();
+            if (is_object($nameOrOther)) {
+                if (method_exists($nameOrOther, 'previousPageUrl') && method_exists($nameOrOther, 'nextPageUrl')) {
+                    // feed 'next' and 'previous' page tags for SEO
+                    $meta = [
+                        'prev' => $nameOrOther->previousPageUrl(),
+                        'next' => $nameOrOther->nextPageUrl(),
+                    ];
+                } else {
+                    // feed related meta rows from the database
+                    $meta = $nameOrOther->meta()->pluck('meta_content', 'meta_name')->toArray();
+                }
             } else {
-                $meta = $nameOrData;
+                // feed by array of a list of key value pairs
+                $meta = $nameOrOther;
             }
         }
+
+        # - $meta is an array holding key and value pairs as meta name and content at this point.
 
         if (!is_array($meta)) {
             throw new InvalidArgumentException('Input must either be an array or a model using "FeedsMeta" trait.');
@@ -65,9 +80,9 @@ class Metaman
                 $meta .= "    <title>$content</title>\n";
                 continue;
             }
-            // use 'link' tag for canonical links
-            if ($name == 'canonical') {
-                $meta .= "    <link rel='canonical' href='$content'/>\n";
+            // use 'link' tag for 'prev', 'next' and 'canonical' SEO directives
+            if (in_array($name, ['prev', 'next', 'content'])) {
+                $meta .= "    <link rel='$name' href='$content'/>\n";
                 continue;
             }
 
